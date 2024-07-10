a00909919b8aab25efa8dc1f15ed679190779650
Olesia <cheban.lesia@gmail.com>
2024-07-10 12:13:34 +0200

    added get available slots

---
+++ SlotBooking.API/Program.cs
@@ -0,0 +1,41 @@
+using SlotBooking.Data;
+using SlotBooking.Domain;
+
+namespace SlotBookingAPI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddHttpClient();
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<ISlotRepository, SlotRepository>();
+        builder.Services.AddScoped<ISlotService, SlotService>();
+        builder.Services.AddRouting();
+        //builder.Services.AddHttpsRedirection();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+        app.UseAuthorization();
+
+        app.Run();
+    }
+}
